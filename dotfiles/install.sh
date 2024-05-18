0c9b0245d76e2e164d2f69ed39cc12b8fb5ac021
Pavel Karpovich <pavel.karpovich@outlook.com>
2024-05-18 20:28:25 +0200

    feat(dotfiles): update gitconfig for GPG signing and add aliases for git commands
    - Enable GPG signing for commits and configure SSH format and program for GPG.
    - Add aliases for common git commands to improve workflow efficiency.
    - Configure global gitignore and install script to symlink dotfiles and tools.
    - Update starship.toml to enhance shell prompt aesthetics and functionality.

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env fish
 
+ln -sf "/Applications/Sublime Merge.app/Contents/SharedSupport/bin/smerge" ~/bin/smerge
+
+ln -sf ~/Projects/environment/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/Projects/environment/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/Projects/environment/dotfiles/starship.toml ~/.config/starship.toml
