21b6c6c5deaf4bfd20318a6c6e6b63677b48d5de
Pavel Karpovich <pavel.karpovich@outlook.com>
2024-05-19 15:45:42 +0200

    feat(dotfiles): integrate mise and zoxide, enhance aliases, and add fish_plugins symlink
    - Add conditional activation and completions for mise in config.fish
    - Replace `mise activate fish` with `zoxide init fish` for directory navigation
    - Introduce new alias for `tree` using `eza`
    - Modify `cat` alias to include `-pp` flag with `bat`
    - Add symlink creation for `fish_plugins` in install.sh to manage fish plugins centrally

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -6,10 +6,33 @@ fish_add_path $PNPM_HOME
 fish_add_path ~/.local/bin
 fish_add_path ~/.local/share/mise/shims
 
-alias ls "eza --color=always --icons --group-directories-first"
+if type -q mise
+    mise activate fish | source
+
+    # if we don't have the completions installed, add them now
+    if ! test -f $HOME/.config/fish/completions/mise.fish
+        mise completions fish > $HOME/.config/fish/completions/mise.fish
+    end
+end
+
+function br --wraps=broot
+    set -l cmd_file (mktemp)
+    if broot --outcmd $cmd_file $argv
+        read --local --null cmd < $cmd_file
+        rm -f $cmd_file
+        eval $cmd
+    else
+        set -l code $status
+        rm -f $cmd_file
+        return $code
+    end
+end
+
+alias ls 'eza --color=always --icons --group-directories-first'
 alias la 'eza --color=always --icons --group-directories-first --all'
 alias ll 'eza --color=always --icons --group-directories-first --all --long'
-alias cat "bat"
+alias cat 'bat -pp'
+alias tree 'eza --tree'
 
-mise activate fish | source
+zoxide init fish | source
 starship init fish | source
--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -6,4 +6,5 @@ ln -sf ~/Projects/environment/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/Projects/environment/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/Projects/environment/dotfiles/mise.config.toml ~/.config/mise/config.toml
 ln -sf ~/Projects/environment/dotfiles/config.fish ~/.config/fish/config.fish
+ln -sf ~/Projects/environment/dotfiles/fish_plugins ~/.config/fish/fish_plugins
 ln -sf ~/Projects/environment/dotfiles/starship.toml ~/.config/starship.toml
